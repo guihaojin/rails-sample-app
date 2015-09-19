c7cc4463a13134f5b5abf47c9fd69b3abbfaa7b4
Haojin Gui <haojin.gui@samsung.com>
2015-09-19 08:51:31 -0700

    typo.

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -16,7 +16,7 @@ class MicropostsController < ApplicationController
   def destroy
     @micropost.destroy
     flash[:success] = "Micropost deleted"
-    redirect_to request.referer || root_url
+    redirect_to request.referrer || root_url
   end
 
   private
